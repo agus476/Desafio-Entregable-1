fcdf75722cdb4784907788e07a381bcbcce8733b
Agustin Sanchez <agu.sanchez15@hotmail.com>
2022-04-27 22:22:59 -0300

    Se agregan cosas

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+class clinic {
+          
+         constructor (name, code , price , schedule) {
+            this.name = name;
+            this.code = code;
+            this.price = price;
+            this.schedule = schedule;
+          }
+         
+        }
+
+        const cardiology = new clinic ("Cardiologia" , 155504, 700 , [10,16,18])
+        const dermatology = new clinic ("Dermatología" , 789546 , 500 )
+        const infectology = new clinic ("Infectología" , 54452. , 1000)
+
+
+
 function menu(){
-    let opcion = prompt("Menu: \n1 - Ver productos y precios \n2 - Visualizar peso por maple \n3 - Canjear codigo de descuento \nESC- Salir")
+    let opcion = prompt("Menu: \n1 - Ver especialidades medicas \n2 - Sacar turno  \n3 - Canjear codigo de descuento \nESC- Salir")
 
     switch(opcion){
         case "1":
-            prices();
-            buyProducts();
+            medicalSp();
+            menu()
             break;
         case "2":
-            weight();
-            menu();
+            turn();
+            
             break;
 
         case "3": 
@@ -26,3 +43,24 @@ function menu(){
 }
 
 menu()
+
+function medicalSp ( ){
+
+alert ("En este hospital contamos con las siguientes especialidades medicas \n-Cardiologia \n-Demartologia \n-Infectologia")
+
+}
+
+function turn ( ){
+
+    let speciality = prompt ( " Seleccione a la especialidad a la cual desea sacar un turno \n-Cardiologia \n-Demartologia \n-Infectologia" )
+
+
+    if (speciality == cardiology.name){
+
+        let hour = prompt ( "Escoja un horario" +  "\n" +  cardiology.schedule[0] + ":00 hs" +"\n" +  cardiology.schedule[1] + ":00 hs" +"\n" +  cardiology.schedule[2] + ":00 hs")
+
+
+    }
+
+    
+}
